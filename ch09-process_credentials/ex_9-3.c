f692f01f4bc0354ac5621ea1a7330afbefd568bc
Trevor Woerner <twoerner@gmail.com>
2012-06-15 15:44:28 -0400

    add ch09 work

---
+++ ch09-process_credentials/ex_9-3.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Implement initgroups() using getgroups() and library functions for retrieving
+ * information from the passwd and group files (Section 8.4). Remember that a
+ * process must be priviledged in order to be able to call setgroups().
+ */
+
+#define _BSD_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int
+main (void)
+{
+	int i;
+	int grpSz;
+	gid_t *grpList_p;
+
+	grpSz = getgroups (0, NULL);
+	if (grpSz == -1) {
+		perror ("getgroups()");
+		return 1;
+	}
+	grpList_p = malloc (grpSz * sizeof (gid_t));
+	if (grpList_p == NULL) {
+		perror ("malloc()");
+		return 1;
+	}
+
+	grpSz = getgroups (grpSz, grpList_p);
+	for (i=0; i<grpSz; ++i)
+		printf ("%ld ", (long)grpList_p[i]);
+	printf ("\n");
+
+	return 0;
+}
---
+++ ch09-process_credentials/listing_9-1.c
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ */
+
+#define _GNU_SOURCE
+#define _USE_GNU
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/fsuid.h>
+#include <limits.h>
+#include <pwd.h>
+#include <grp.h>
+#include <ctype.h>
+
+#define SG_SIZE (NGROUPS_MAX + 1)
+
+char *
+user_name_from_id (uid_t uid)
+{
+	struct passwd *pwd_p;
+
+	pwd_p = getpwuid (uid);
+	return (pwd_p == NULL)? NULL : pwd_p->pw_name;
+}
+
+char *
+group_name_from_id (gid_t gid)
+{
+	struct group *grp_p;
+
+	grp_p = getgrgid (gid);
+	return (grp_p == NULL)? NULL : grp_p->gr_name;
+}
+
+int
+main (void)
+{
+	uid_t ruid, euid, suid, fsuid;
+	gid_t rgid, egid, sgid, fsgid;
+	gid_t suppGroups[SG_SIZE];
+	int numGroups, j;
+	char *ptr;
+
+	if (getresuid (&ruid, &euid, &suid) == -1) {
+		perror ("getresuid()");
+		return 1;
+	}
+	if (getresgid (&rgid, &egid, &sgid) == -1) {
+		perror ("getresgid()");
+		return 1;
+	}
+
+	/*
+	   attempts to change the file-system IDs are always ignored
+	   for unprivileged processes, but even so, the following
+	   calls return the current file-system IDs
+	*/
+	fsuid = setfsuid (0);
+	fsgid = setfsgid (0);
+
+	printf ("UID: ");
+	ptr = user_name_from_id (ruid);
+	printf ("real=%s (%ld); ", ptr?:"?", (long)ruid);
+	ptr = user_name_from_id (euid);
+	printf ("eff=%s (%ld); ", ptr?:"?", (long)euid);
+	ptr = user_name_from_id (suid);
+	printf ("saved=%s (%ld); ", ptr?:"?", (long)suid);
+	ptr = user_name_from_id (fsuid);
+	printf ("fs=%s (%ld);", ptr?:"?", (long)fsuid);
+	printf ("\n");
+
+	printf ("GID: ");
+	ptr = group_name_from_id (rgid);
+	printf ("real=%s (%ld); ", ptr?:"?", (long)rgid);
+	ptr = group_name_from_id (egid);
+	printf ("eff=%s (%ld); ", ptr?:"?", (long)egid);
+	ptr = group_name_from_id (sgid);
+	printf ("saved=%s (%ld); ", ptr?:"?", (long)sgid);
+	ptr = group_name_from_id (fsgid);
+	printf ("fs=%s (%ld);\n", ptr?:"?", (long)fsgid);
+
+	numGroups = getgroups (SG_SIZE, suppGroups);
+	if (numGroups == -1) {
+		perror ("getgroups()");
+		return 1;
+	}
+	printf ("supplementary groups (%d): ", numGroups);
+	for (j=0; j<numGroups; ++j) {
+		ptr = group_name_from_id (suppGroups[j]);
+		printf ("%s (%ld) ", ptr?:"?", (long)suppGroups[j]);
+	}
+	printf ("\n");
+
+	return 0;
+}
