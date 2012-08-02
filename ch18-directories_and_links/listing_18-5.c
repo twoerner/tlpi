892c3b5e0a87bbc57e3fbafaa3bccc894b931951
Trevor Woerner <twoerner@gmail.com>
2012-08-01 20:24:58 -0400

    ch 18: listing 18-5

---
+++ ch18-directories_and_links/listing_18-5.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Demonstrate usage of dirname(3) and basename(3).
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libgen.h>
+
+int
+main (int argc, char *argv[])
+{
+	char *t1_p, *t2_p;
+	int i;
+
+	for (i=1; i<argc; ++i) {
+		t1_p = strdup (argv[i]);
+		if (t1_p == NULL) {
+			perror ("strdup()");
+			return 1;
+		}
+
+		t2_p = strdup (argv[i]);
+		if (t2_p == NULL) {
+			perror ("strdup()");
+			return 1;
+		}
+
+		printf ("%s ==> %s + %s\n", argv[i], dirname (t1_p), basename (t2_p));
+
+		free (t1_p);
+		free (t2_p);
+	}
+
+	return 0;
+}
