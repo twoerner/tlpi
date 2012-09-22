781e3560681366dbb083cda18e68cee0ec3fe436
Trevor Woerner <twoerner@gmail.com>
2012-09-22 12:59:36 -0400

    ch 20: listings 20-6 and 20-7

---
+++ ch20-signals_fundamental_concepts/listing_20-4.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Helper routines for displaying signal sets
+ */
+
+#ifndef __LISTING_20_4__H
+#define __LISTING_20_4__H
+
+void print_signal_set (FILE *outFile_p, const char *prefix_p, const sigset_t *sigset_p);
+int print_signal_mask (FILE *outFile_p, const char *msg_p);
+int print_pending_signals (FILE *outFile_p, const char *msg_p);
+
+#endif
---
+++ ch20-signals_fundamental_concepts/listing_20-6.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * This program can be used to observe that signals are not queued. This
+ * program takes up to 4 cmdline args:
+ *
+ *	$ ./listing_20-6 <pid> <signal count> <signal> [<signal2>]
+ *
+ * The first argument is the process ID of the process to which the program
+ * should send signals. The second argument specifies the number of signals
+ * to be sent to the target process.  The third argument specifies the signal
+ * that is to be sent to the target process. If the fourth argument is given,
+ * this program sends one instance of that signal to the process after sending
+ * the signals specified by the second and third cmdline args.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+
+int
+main (int argc, char *argv[])
+{
+	int numSigs, sig, i;
+	pid_t pid;
+
+	if (argc < 4 || strcmp (argv[1], "--help") == 0) {
+		printf ("usage: %s <pid> <signal count> <signal> [<signal2>]\n", argv[0]);
+		return 1;
+	}
+
+	pid = atol (argv[1]);
+	numSigs = atoi (argv[2]);
+	if (numSigs <= 0) {
+		printf ("please specify a positive integer for option 2 <signal count>\n");
+		return 1;
+	}
+	sig = atoi (argv[3]);
+
+	printf ("%s: sending signal %d to process %ld %d times\n",
+			argv[0], sig, (long)pid, numSigs);
+	for (i=0; i<numSigs; ++i)
+		if (kill (pid, sig) == -1) {
+			perror ("kill()");
+			return 1;
+		}
+
+	if (argc > 4) {
+		sig = atoi (argv[4]);
+		if (kill (pid, sig) == -1) {
+			perror ("kill()");
+			return 1;
+		}
+	}
+
+	return 0;
+}
