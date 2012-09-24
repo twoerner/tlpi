37a9ca2e7826a1aefea82c9c2a3c0562ce6371cc
Trevor Woerner <twoerner@gmail.com>
2012-09-24 07:04:42 -0400

    ch 20: exercise 20-3 (b)

---
+++ ch20-signals_fundamental_concepts/ex_20-3b.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Write programs that verify the effect of the SA_RESETHAND and SA_NODEFER
+ * flags when establishing a signal handler with sigaction().
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+static void handler (int);
+
+int
+main (void)
+{
+	int ret;
+	struct sigaction sa;
+
+	memset (&sa, 0, sizeof (sa));
+	ret = sigemptyset (&sa.sa_mask);
+	if (ret != 0) {
+		perror ("sigemptyset()");
+		return 1;
+	}
+
+	sa.sa_handler = handler;
+	sa.sa_flags = SA_NODEFER;
+	ret = sigaction (SIGINT, &sa, NULL);
+	if (ret != 0) {
+		perror ("sigaction()");
+		return 1;
+	}
+
+	while (1)
+		pause ();
+
+	return 0;
+}
+
+static void
+handler (int __attribute__((unused)) sig)
+{
+	fprintf (stderr, "in ");
+	sleep (2);
+	fprintf (stderr, "a ");
+	sleep (2);
+	fprintf (stderr, "very ");
+	sleep (2);
+	fprintf (stderr, "slow ");
+	sleep (2);
+	fprintf (stderr, "handler ");
+	sleep (2);
+}
