f93f210b4fa0fa2f0ff733cda9c181b1998e4118
Trevor Woerner <twoerner@gmail.com>
2012-09-28 17:20:41 -0400

    ch 22: exercise 22-3
    The two programs necessary to test the claim in example 22-3.

---
+++ ch22-signals__advanced_features/ex_22-3b.c
@@ -0,0 +1,126 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Section 22.10 stated that accepting signals using sigwaitinfo() is faster
+ * than the use of a signal handler plus sigsuspend(). Write two programs
+ * to implement each approach which exchange a million signals between a
+ * parent a child to validate this claim.
+ *
+ * This solution uses a signal handler + sigsuspend().
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/types.h>
+
+#define UNUSED __attribute__((unused))
+
+static void run_child (void);
+static void run_parent (pid_t childPid);
+
+static void
+signal_handler (UNUSED int sig)
+{
+}
+
+int
+main (void)
+{
+	pid_t pid;
+	struct sigaction sa;
+	sigset_t sigSet;
+
+	// setup signal handler
+	sigemptyset (&sa.sa_mask);
+	sa.sa_flags = 0;
+	sa.sa_handler = signal_handler;
+	if (sigaction (SIGUSR1, &sa, NULL) == -1) {
+		perror ("sigaction()");
+		return 1;
+	}
+
+	// block SIGUSR1 to start
+	if (sigemptyset (&sigSet) == -1) {
+		perror ("sigemptyset()");
+		return 1;
+	}
+	if (sigaddset (&sigSet, SIGUSR1) == -1) {
+		perror ("sigaddset()");
+		return 1;
+	}
+	if (sigprocmask (SIG_SETMASK, &sigSet, NULL) == -1) {
+		perror ("sigprocmask()");
+		return 1;
+	}
+
+	pid = fork ();
+	switch (pid) {
+		case -1: // error
+			perror ("fork()");
+			return 1;
+
+		case 0: // child
+			run_child ();
+			break;
+
+		default: // parent
+			run_parent (pid);
+			break;
+	}
+
+	return 0;
+}
+
+static void
+run_child (void)
+{
+	sigset_t sigSet;
+	pid_t pid;
+
+	if (sigemptyset (&sigSet) == -1) {
+		perror ("sigemptyset()");
+		exit (1);
+	}
+
+	pid = getppid ();
+
+	while (1) {
+		if ((sigsuspend (&sigSet) == -1) && (errno != EINTR)) {
+			perror ("child sigsuspend()");
+			exit (1);
+		}
+		if (kill (pid, SIGUSR1) == -1) {
+			perror ("child kill()");
+			exit (1);
+		}
+	}
+}
+
+static void
+run_parent (pid_t childPid)
+{
+	unsigned i;
+	sigset_t sigSet;
+
+	if (sigemptyset (&sigSet) == -1) {
+		perror ("sigemptyset()");
+		kill (childPid, SIGINT);
+		exit (1);
+	}
+
+	for (i=0; i<1000000; ++i) {
+		if (kill (childPid, SIGUSR1) == -1) {
+			perror ("parent kill()");
+			break;
+		}
+		if ((sigsuspend (&sigSet) == -1) && (errno != EINTR)) {
+			perror ("parent sigsuspend()");
+			break;
+		}
+	}
+
+	kill (childPid, SIGINT);
+}
