c028143d143c4e83ee5f756300f19ef74cd4727e
Trevor Woerner <twoerner@gmail.com>
2012-09-27 23:28:15 -0400

    ch 22: exercise 22-1

---
+++ ch22-signals__advanced_features/ex_22-1.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Section 22.2 noted that if a stopped process that has established a handler
+ * for and blocked SIGCONT is later resumed as a consequence of receiving a
+ * SIGCONT, then the handler is invoked only when SIGCONT is unblocked. Write
+ * a program to verify this. Recall that a process can be stopped by typing
+ * the terminal suspend character (usually Ctrl-Z) and can be sent a SIGCONT
+ * signal using either "kill -CONT" or using the shell "fg" command.
+ */
+
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void
+handler (int sig)
+{
+	printf ("caught signal %d\n", sig);
+}
+
+int
+main (void)
+{
+	sigset_t block;
+	struct sigaction sa;
+
+	if (sigemptyset (&block) == -1) {
+		perror ("sigemptyset()");
+		return 1;
+	}
+	if (sigaddset (&block, SIGCONT) == -1) {
+		perror ("sigaddset()");
+		return 1;
+	}
+	if (sigprocmask (SIG_BLOCK, &block, NULL) == -1) {
+		perror ("sigprocmask()");
+		return 1;
+	}
+
+	if (sigemptyset (&sa.sa_mask) == -1) {
+		perror ("sigemptyset (sa_mask)");
+		return 1;
+	}
+	sa.sa_flags = 0;
+	sa.sa_handler = handler;
+	if (sigaction (SIGCONT, &sa, NULL) == -1) {
+		perror ("sigaction()");
+		return 1;
+	}
+
+	fprintf (stderr, "sleeping... ");
+	sleep (60);
+	fprintf (stderr, "done\n");
+
+	if (sigprocmask (SIG_UNBLOCK, &block, NULL) == -1) {
+		perror ("sigprocmask (2)");
+		return 1;
+	}
+
+	return 0;
+}
