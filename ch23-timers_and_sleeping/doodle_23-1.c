2f0f984e344b1dc243da98e6c8fbc788f664cbfe
Trevor Woerner <twoerner@gmail.com>
2012-09-28 19:45:22 -0400

    ch 23: doodle 23-1

---
+++ ch23-timers_and_sleeping/doodle_23-1.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Is it possible to pass NULL for new_value in setitimer()?
+ */
+
+#include <stdio.h>
+#include <sys/time.h>
+
+int
+main (void)
+{
+	int ret;
+	struct itimerval old;
+
+	ret = setitimer (ITIMER_REAL, NULL, &old);
+	if (ret == -1) {
+		perror ("setitimer()");
+		return 1;
+	}
+	printf ("old.it_interval.tv_sec:  %ld\n", old.it_interval.tv_sec);
+	printf ("old.it_interval.tv_usec: %ld\n", old.it_interval.tv_usec);
+	printf ("old.it_value.tv_sec:     %ld\n", old.it_value.tv_sec);
+	printf ("old.it_value.tv_usec:    %ld\n", old.it_value.tv_usec);
+
+	return 0;
+}
