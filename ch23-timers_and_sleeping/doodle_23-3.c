e357f9b7f28d308dee1e2a7690f3cfacaeed110f
Trevor Woerner <twoerner@gmail.com>
2012-10-01 18:02:01 -0400

    ch 23: doodle 23-3

---
+++ ch23-timers_and_sleeping/doodle_23-3.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Questions about clockid_t.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+
+int
+main (void)
+{
+	clockid_t clockId;
+
+	printf ("CLOCK_REALTIME............%ld\n", (long)CLOCK_REALTIME);
+	printf ("CLOCK_MONOTONIC...........%ld\n", (long)CLOCK_MONOTONIC);
+	printf ("CLOCK_PROCESS_CPUTIME_ID..%ld\n", (long)CLOCK_PROCESS_CPUTIME_ID);
+	printf ("CLOCK_THREAD_CPUTIME_ID...%ld\n", (long)CLOCK_THREAD_CPUTIME_ID);
+
+	clock_getcpuclockid (0, &clockId);
+	printf ("clock ID of process 0.....%ld\n", (long)clockId);
+
+	clock_getcpuclockid (getpid (), &clockId);
+	printf ("clock ID of getpid()......%ld\n", (long)clockId);
+
+	return 0;
+}
