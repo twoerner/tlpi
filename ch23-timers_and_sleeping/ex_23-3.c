c79f49a8cce70ebb29f1d6086a63233dba1bc3cd
Trevor Woerner <twoerner@gmail.com>
2012-10-02 13:28:52 -0400

    ch 23: exercise 23-3

--- ch23-timers_and_sleeping/ex_23-3.c
+++ ch23-timers_and_sleeping/ex_23-3.c
@@ -7,3 +7,55 @@
  * set to SIGALRM, and si_value.sival_int set to the timer ID.
  */
 
+#include <stdio.h>
+#include <string.h>
+#include <signal.h>
+#include <time.h>
+#include <unistd.h>
+
+// UNSAFE: printf(), strsignal()
+static void
+signal_handler (int sig, siginfo_t *sigInfo_p, __attribute__((unused)) void *ctx_p)
+{
+	printf ("caught signal %d (%s)\n", sig, strsignal (sig));
+	printf ("\tsi_value.sival_int: %ld\n", (long)sigInfo_p->si_value.sival_int);
+}
+
+int
+main (void)
+{
+	int ret;
+	timer_t tid;
+	struct sigaction sa;
+	struct itimerspec newVal;
+
+	sigemptyset (&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigaction (SIGALRM, &sa, NULL);
+
+	ret = timer_create (CLOCK_REALTIME, NULL, &tid);
+	if (ret == -1) {
+		perror ("timer_create()");
+		return 1;
+	}
+	printf ("main(): tid = %ld\n", (long)tid);
+
+	memset (&newVal, 0, sizeof (newVal));
+	newVal.it_value.tv_sec = 3;
+	ret = timer_settime (tid, 0, &newVal, NULL);
+	if (ret == -1) {
+		perror ("timer_settime()");
+		return 1;
+	}
+
+	pause ();
+
+	ret = timer_delete (tid);
+	if (ret == -1) {
+		perror ("timer_delete()");
+		return 1;
+	}
+
+	return 0;
+}
