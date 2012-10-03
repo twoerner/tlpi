3ff492f92371efc593de4c1206d3990fa5c6e97f
Trevor Woerner <twoerner@gmail.com>
2012-10-03 15:04:59 -0400

    ch 24: doodle 24-1

---
+++ ch24-process_creation/doodle_24-1.c
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+// set to 0 or 1
+#define FORGET_EXIT 0
+
+int
+main (void)
+{
+	pid_t pid;
+
+	printf ("parent\n");
+	pid = vfork();
+
+	if (pid == 0) {
+		printf ("child\n");
+#if (FORGET_EXIT == 0)
+		_exit (0);
+#endif
+	}
+	return 0;
+}
