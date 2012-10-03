82e7cdd7f31eaba43164aa45144dabb9fd0bec03
Trevor Woerner <twoerner@gmail.com>
2012-10-03 16:11:23 -0400

    ch 24: listing 24-5

---
+++ ch24-process_creation/listing_24-5.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Demonstrate who (the parent or the child) gets to run first
+ * after a call to fork().
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int
+main (int argc, char *argv[])
+{
+	int numCh, i;
+	pid_t chldPid;
+
+	if (argc > 1 && strcmp (argv[1], "--help") == 0) {
+		printf ("usage: %s [<number of children>]\n", argv[0]);
+		return 0;
+	}
+
+	numCh = (argc > 1)? atoi (argv[1]) : 1;
+	if (numCh < 1) {
+		printf ("please specify a positive, non-zero, number of children\n");
+		return 1;
+	}
+	setbuf (stdout, NULL);
+
+	for (i=0; i<numCh; ++i) {
+		switch (chldPid = fork ()) {
+			case -1:
+				perror ("fork()");
+				return 1;
+
+			case 0: // child
+				printf ("%d child\n", i);
+				_exit (0);
+
+			default: // parent
+				printf ("%d parent\n", i);
+				wait (NULL);
+				break;
+		}
+	}
+
+	return 0;
+}
