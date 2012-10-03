ec6d593e56f79bf524aacd01d7946d2f92e578ca
Trevor Woerner <twoerner@gmail.com>
2012-10-03 17:43:38 -0400

    ch 25: listing 25-1

---
+++ ch25-process_termination/listing_25-1.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Demonstrate the use of atexit() and on_exit().
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// set to 0 or 1
+#define USE_EXIT 0
+
+#define ATEXIT(n) \
+	static void \
+	atexit_fn##n (void) \
+	{ \
+		printf ("%s called\n", __func__); \
+	}
+
+ATEXIT(1)
+ATEXIT(2)
+
+static void
+on_exit_fn (int exitStatus, void *arg_p)
+{
+	printf ("%s called\n\texit status: %d\n\targ: %ld\n", __func__, exitStatus, (long)arg_p);
+}
+
+int
+main (void)
+{
+	if (on_exit (on_exit_fn, (void*)10) != 0) {
+		perror ("on_exit_fn");
+		_exit (1);
+	}
+	if (atexit (atexit_fn1) != 0) {
+		perror ("atexit_fn1");
+		_exit (1);
+	}
+	if (atexit (atexit_fn2) != 0) {
+		perror ("atexit_fn2");
+		_exit (1);
+	}
+	if (on_exit (on_exit_fn, (void*)20) != 0) {
+		perror ("on_exit_fn");
+		_exit (1);
+	}
+
+#if (USE_EXIT == 1)
+	exit (2);
+#else
+	return 3;
+#endif
+}
