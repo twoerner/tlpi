fe41edf776b9a9300af9b5e7fecc4cec89612959
Trevor Woerner <twoerner@gmail.com>
2012-10-03 14:28:30 -0400

    ch 24: listing 24-4

---
+++ ch24-process_creation/listing_24-4.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Demonstration of vfork().
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int
+main (void)
+{
+	int istack = 222;
+
+	switch (vfork ()) {
+		case -1:
+			perror ("vfork()");
+			return 1;
+
+		case 0: // child
+			sleep (3);
+
+			printf ("child\n");
+			istack *= 3;
+			_exit (0);
+
+		default:
+			printf ("parent\n");
+			printf ("\tistack: %d\n", istack);
+	}
+
+	return 0;
+}
