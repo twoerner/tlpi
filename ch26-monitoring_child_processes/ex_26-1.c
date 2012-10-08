1a95c4d59fb011e099d0568ece45daca5da45819
Trevor Woerner <twoerner@gmail.com>
2012-10-08 15:58:52 -0400

    ch 26: exercise 26-1

---
+++ ch26-monitoring_child_processes/ex_26-1.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Write a program to verify that when a child's parent terminates, a call to
+ * getpid() returns 1 (i.e. the child process was orphaned and adopted by the
+ * init process).
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+
+int
+main (void)
+{
+	switch (fork ()) {
+		case -1:
+			perror ("fork()");
+			return 1;
+
+		case 0: // child
+			sleep (5);
+			printf ("parent: %ld\n", (long)getppid ());
+			_exit (0);
+
+		default:
+			break;
+	}
+
+	return 0;
+}
