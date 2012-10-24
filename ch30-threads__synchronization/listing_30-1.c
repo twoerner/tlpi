cc924ed42e57be5879456b4b700f1990f2fcc874
Trevor Woerner <twoerner@gmail.com>
2012-10-24 13:01:03 -0400

    ch 30: listing 30-1

---
+++ ch30-threads__synchronization/listing_30-1.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Incorrectly incrementing a global variable from two threads.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+static volatile int global_G = 0;
+
+static void *
+thread_func (void *arg_p)
+{
+	int loops = *((int*)arg_p);
+	int local, i;
+
+	for (i=0; i<loops; ++i) {
+		local = global_G;
+		++local;
+		global_G = local;
+	}
+
+	return NULL;
+}
+
+int
+main (int argc, char *argv[])
+{
+	pthread_t t1, t2;
+	int loops, ret;
+
+	loops = (argc > 1)? atoi (argv[1]) : 10000000;
+	if (loops < 1) {
+		printf ("please specify a positive loop value\n");
+		return 1;
+	}
+
+	ret = pthread_create (&t1, NULL, thread_func, &loops);
+	if (ret != 0) {
+		perror ("pthread_create (t1)");
+		return 1;
+	}
+	ret = pthread_create (&t2, NULL, thread_func, &loops);
+	if (ret != 0) {
+		perror ("pthread_create (t2)");
+		return 1;
+	}
+
+	ret = pthread_join (t1, NULL);
+	if (ret != 0) {
+		perror ("pthread_join (t1)");
+		return 1;
+	}
+	ret = pthread_join (t2, NULL);
+	if (ret != 0) {
+		perror ("pthread_join (t2)");
+		return 1;
+	}
+
+	printf ("global_G: %d\n", global_G);
+	return 0;
+}
