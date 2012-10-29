3e283defa23fe11d008603d9695427992d185730
Trevor Woerner <twoerner@gmail.com>
2012-10-29 16:01:59 -0400

    ch 31: listing 31-4

---
+++ ch31-threads__thread_safety_and_per_thread_storage/listing_31-4.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2012  Trevor Woerner
+ *
+ * Non-thread-safe implementation of strerror ().
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <pthread.h>
+
+#define MAX_ERROR_LEN 256
+static __thread char buf_G[MAX_ERROR_LEN];
+
+static char *
+my_strerror (int err)
+{
+	if (err < 0 || err >= _sys_nerr || _sys_errlist[err] == NULL)
+		snprintf (buf_G, sizeof (buf_G), "unknown error %d", err);
+	else {
+		strncpy (buf_G, _sys_errlist[err], MAX_ERROR_LEN - 1);
+		buf_G[MAX_ERROR_LEN - 1] = 0;
+	}
+
+	return buf_G;
+}
+
+static void *
+thread_func (__attribute__((unused)) void *arg)
+{
+	char *str_p;
+
+	printf ("other thread about to call strerror()\n");
+	str_p = my_strerror (EPERM);
+	printf ("other thread: str (%p) = %s\n", str_p, str_p);
+
+	return NULL;
+}
+
+int
+main (void)
+{
+	pthread_t t;
+	int ret;
+	char *str_p;
+
+	str_p = my_strerror (EINVAL);
+	printf ("main thread has called strerror()\n");
+
+	ret = pthread_create (&t, NULL, thread_func, NULL);
+	if (ret != 0) {
+		perror ("pthread_create()");
+		return 1;
+	}
+
+	ret = pthread_join (t, NULL);
+	if (ret != 0) {
+		perror ("pthread_join()");
+		return 1;
+	}
+
+	printf ("main thread: str (%p) = %s\n", str_p, str_p);
+	return 0;
+}
