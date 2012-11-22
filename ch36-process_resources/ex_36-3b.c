5f715115c3e73d3c406f8592cef149f03270cae4
Trevor Woerner <twoerner@gmail.com>
2012-11-22 11:26:48 -0500

    exercise 36-3b: updates
    - show the amount of "work" getting done in the busy loop
    - allow the user to select on the cmdline whether or not to try
      to reset the RLIMIT_CPU (for comparison)

--- ch36-process_resources/ex_36-3b.c
+++ ch36-process_resources/ex_36-3b.c
@@ -18,58 +18,70 @@
 #include "print_rlimit.h"
 
 volatile sig_atomic_t flag_G;
+volatile sig_atomic_t rlimSet_G = 0;
+struct rlimit rlim_G;
+volatile sig_atomic_t resetLimits_G = 0;
 
 static void
 signal_handler (int sig)
 {
 	flag_G = 1;
-	printf ("signal %d (%s) caught\n", sig, strsignal (sig));
+	printf ("\nsignal %d (%s) caught\n", sig, strsignal (sig));
 	print_rlimit (RLIMIT_CPU);
+
+	if (resetLimits_G)
+		if (rlimSet_G == 1)
+			setrlimit (RLIMIT_CPU, &rlim_G);
 }
 
 int
-main (void)
+main (int argc, __attribute__((unused)) char *argv[])
 {
 	int ret;
 	struct sigaction sa;
-	unsigned sig;
-	struct rlimit rlim;
 	volatile unsigned i, j;
+
+	// check cmdline args
+	if (argc > 1)
+		resetLimits_G = 1;
 
 	// setup signal handler
 	flag_G = 0;
 	sigemptyset (&sa.sa_mask);
 	sa.sa_flags = 0;
 	sa.sa_handler = signal_handler;
-	for (sig=1; sig<_NSIG; ++sig) {
-		ret = sigaction (sig, &sa, NULL);
-		if (ret == -1)
-			printf ("can't sigaction signal %2d (%s)\n", sig, strsignal (sig));
+	ret = sigaction (SIGXCPU, &sa, NULL);
+	if (ret == -1) {
+		printf ("can't sigaction signal %2d (%s)\n", SIGXCPU, strsignal (SIGXCPU));
+		perror ("sigaction()");
+		return 1;
 	}
-	printf ("\n");
 
 	// set rlimits
 	printf ("before ");
 	print_rlimit (RLIMIT_CPU);
 
-	rlim.rlim_cur = 2;
-	rlim.rlim_max = 8;
-	ret = setrlimit (RLIMIT_CPU, &rlim);
+	rlim_G.rlim_cur = 2;
+	rlim_G.rlim_max = 8;
+	ret = setrlimit (RLIMIT_CPU, &rlim_G);
 	if (ret == -1) {
 		perror ("setrlimit()");
 		return 1;
 	}
+	rlimSet_G = 1;
 
 	printf ("after  ");
 	print_rlimit (RLIMIT_CPU);
 
 	// busy loop
-	for (i=0; i<1000000; ++i)
+	for (i=0; i<1000000; ++i) {
+		fprintf (stderr, ".");
 		for (j=0; j<1000000; ++j)
 			if (flag_G) {
 				flag_G = 0;
 				sleep (1);
 			}
+	}
 
 	return 0;
 }
