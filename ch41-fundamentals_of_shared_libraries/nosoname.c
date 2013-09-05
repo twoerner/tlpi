20f1ee22ef3b188477a878b59f114d766f02ab4e
Trevor Woerner <twoerner@gmail.com>
2013-09-05 15:37:08 -0400

    ch 41: no soname
    If you create a shared object and don't provide a soname, the object's name
    becomes the soname. If the object's name contains specific version information
    then whatever needs this object can only ever link to this object's specific
    name as it existed when the compile was performed.
    
    In other words, the flexibility of being able to upgrade minor versions of an
    object is lost.
    
    For example, if you create an object called libnsn.so.2.0.6, it does not
    contain a soname, and link an application to it, the application's NEEDED
    section will specifically list libnsn.so.2.0.6 which means future (or older)
    version 2's of libnsn will not be sufficient to allow the application to run;
    the application will only ever run with libnsn.so.2.0.6.

---
+++ ch41-fundamentals_of_shared_libraries/nosoname.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2013  Trevor Woerner <twoerner@gmail.com>
+ */
+
+#include <stdio.h>
+
+int my_func (int, int);
+
+int
+main (void)
+{
+	printf ("my_func (4, 12) yields: %d\n", my_func (4, 12));
+	return 0;
+}
